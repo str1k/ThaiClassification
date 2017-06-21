63f8b6945f3960953caeca025a115a0da4947367
Thanasit Yiamwinya <t-thyiam@microsoft.com>
2017-06-21 15:14:36 +0700

    Test api server5

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var fs = require("fs");
+var wordcut = require("wordcut");
 
 app.get('/listUsers', function (req, res) {
    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
@@ -8,7 +9,18 @@ app.get('/listUsers', function (req, res) {
        res.end( data );
    });
 })
-
+app.get('/:sentence', function (req, res) {
+	wordcut.init();
+	console.log(wordcut.cut(req.params.sentence));
+	res.end(wordcut.cut(req.params.sentence));
+   // First read existing users.
+   //fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+   //    users = JSON.parse( data );
+   //    var user = users["user" + req.params.id] 
+   //    console.log( user );
+   //    res.end( JSON.stringify(user));
+   //});
+})
 var server = app.listen(process.env.PORT || 1337, function () {
 
   var host = server.address().address
