c5224d1ae9fae20e25ab3d0614aba4462f622a78
Thanasit Yiamwinya <t-thyiam@microsoft.com>
2017-06-21 15:20:20 +0700

    First SegmentationAPI

--- index.js
+++ index.js
@@ -12,14 +12,11 @@ app.get('/listUsers', function (req, res) {
 app.get('/:sentence', function (req, res) {
 	wordcut.init();
 	console.log(wordcut.cut(req.params.sentence));
-	res.end(wordcut.cut(req.params.sentence));
-   // First read existing users.
-   //fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-   //    users = JSON.parse( data );
-   //    var user = users["user" + req.params.id] 
-   //    console.log( user );
-   //    res.end( JSON.stringify(user));
-   //});
+	var body = wordcut.cut(req.params.sentence);
+	
+	res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
+    res.write(body, "utf-8");
+    res.end(); 
 })
 var server = app.listen(process.env.PORT || 1337, function () {
 
